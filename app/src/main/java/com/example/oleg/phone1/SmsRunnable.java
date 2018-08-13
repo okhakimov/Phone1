1b69242d447b092970e2444995134f14ae1b58b7
okhakimov <okhakimov@gmail.com>
2018-08-13 22:21:07 +0400

    13/08/2018 Improvement. Broadcast receiver added to SmsRunnable to check if sms sent. A sound is played if the sending was ok 13/08/2018 Feature/Improvement. Class SmsRunnable created to send sms with delay in seconds definde by option sms_delay in the config file. 11/08/2018 Feature. Kode "o" for options added to configuration file. Two options: "test" and "sms_delay" 06/08/2018 Fixed bug in assigning text of the buttons and call numbers because of not using params.CallId parameter ------------ TODO: - ?? Fix. Change shapes of sms buttons back to rounded - clean code - test different config files
    ------------
    History:
    
    - Added check of reading and parsing of the input file. Error message is displayed as a text of button call1 if unsucessfull.
    
    TODO:
    - (Done 13/08/2018( Improvement. Check if sms was sent. A sound is played if sent was ok.
    - (Done 13/08/2018) Feature. Send sms to different phones with time interval of sms_delay option (sec)
    - (Done 11/08/2018) Add option in the config file to switch on and off a testing mode (instead of calling and sending sms, messages will be displayed)
    - (Done 11/08_ Add an option for delays between sms for several numbers

--- app/src/main/java/com/example/oleg/phone1/SmsRunnable.java
+++ app/src/main/java/com/example/oleg/phone1/SmsRunnable.java
@@ -1,6 +1,11 @@
 package com.example.oleg.phone1;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -24,6 +29,31 @@ public class SmsRunnable implements Runnable {
     }
     @Override
     public void run() {
+        String sent = "SMS_SENT";
+        PendingIntent sentPI = PendingIntent.getBroadcast(context, 0,
+                new Intent(sent), 0);
+
+        //---when the SMS has been sent---
+        context.registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                if(getResultCode() == Activity.RESULT_OK)
+                {
+                    Toast.makeText(context, "SMS sent",
+                            Toast.LENGTH_SHORT).show();
+                    MediaPlayer mp_sms_sent = MediaPlayer.create(context, R.raw.bell1);
+                    mp_sms_sent.start();
+                }
+                else
+                {
+                    Toast.makeText(context, "SMS could not sent",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new IntentFilter(sent));
+
+
+
         //String phone = params.Phones[global_i[count]][1];
         //Log.d("=== r ",phone);
         if (test) {
@@ -31,7 +61,7 @@ public class SmsRunnable implements Runnable {
         } else {
             //send sms
             SmsManager sms = SmsManager.getDefault();
-            sms.sendTextMessage(phone, null, message, null, null);
+            sms.sendTextMessage(phone, null, message, sentPI, null);
             Toast.makeText(context, "== sms " + phone + " " + message, Toast.LENGTH_LONG).show();
 
         }
